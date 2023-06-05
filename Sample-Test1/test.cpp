3c1859f66b726200c7d4d76457615db871fbf890
oj <ozoppp@gmail.com>
2023-06-05 15:30:05 +0900

    read 구현

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,51 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../TDD_DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+using namespace std;
+
+class MockFlashMemoryDevice : public FlashMemoryDevice
+{
+public:
+    MOCK_METHOD(unsigned char, read, (long address), (override));
+    MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(ReadTest, ReadFlash5Times) {
+    MockFlashMemoryDevice cMockFlash;
+
+    EXPECT_CALL(cMockFlash, read(_))
+        .Times(5);
+
+    DeviceDriver cDeviceDriver(&cMockFlash);
+
+    cDeviceDriver.read(0x0);
 }
+
+TEST(ReadTest, ReadFailException) {
+    MockFlashMemoryDevice cMockFlash;
+
+    EXPECT_CALL(cMockFlash, read(_))
+        .WillOnce(Return(0x5))
+        .WillOnce(Return(0xFF))
+        .WillOnce(Return(0x5))
+        .WillOnce(Return(0x3))
+        .WillOnce(Return(0x5));
+
+    DeviceDriver cDeviceDriver(&cMockFlash);
+
+    EXPECT_THROW(cDeviceDriver.read(0x0), exception);
+}
+
+TEST(ReadTest, ReadDone) {
+    MockFlashMemoryDevice cMockFlash;
+
+    EXPECT_CALL(cMockFlash, read(_))
+        .WillRepeatedly(Return(0x7));
+
+    DeviceDriver cDeviceDriver(&cMockFlash);
+
+    EXPECT_THAT(0x7, cDeviceDriver.read(0x0));
+}
+
--- TDD_DeviceDriver/DeviceDriver.cpp
+++ TDD_DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,30 @@
 #include "DeviceDriver.h"
+#include <windows.h>
+#include <cstdio>
+#include <stdexcept>
+#include <system_error>
+
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int nResult[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        Sleep(1);
+        nResult[i] = (int)(m_hardware->read(address));
+    }
+
+    for (int i = 1; i < 5; i++)
+    {
+        if (nResult[0] != nResult[i]) throw exception("ReadFailException");
+    }
+
+    return nResult[0];
 }
 
 void DeviceDriver::write(long address, int data)
